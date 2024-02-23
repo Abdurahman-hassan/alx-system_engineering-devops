ccc99882603a677f08cf67689a27584a397dc68f
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-24 01:37:58 +0200

    Add comment in the first line

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,4 +1,7 @@
+# Create a file with the content "I love Puppet" in /tmp/school with the following permissions: 0744, owned by www-data:www-data.
 file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
