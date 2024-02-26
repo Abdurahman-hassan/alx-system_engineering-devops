219cc0b340fb3a425a9e74224a16d9f3d1c32da6
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-02-26 04:21:40 +0200

    fix puppet script

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,28 +1,13 @@
-# Ensure the SSH client config directory exists
-file { '/home/ubuntu/.ssh':
-  ensure => directory,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0700',
-}
+# Puppet manifest to configure SSH client settings
 
-# Ensure the SSH client config file exists
-file { '/home/ubuntu/.ssh/config':
-  ensure => file,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0600',
-}
-
-# Configure SSH to use the private key and refuse password authentication
 file_line { 'Declare identity file':
-  path => '/home/ubuntu/.ssh/config',
-  line => 'IdentityFile ~/.ssh/school',
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
 
 file_line { 'Turn off passwd auth':
-  path    => '/home/ubuntu/.ssh/config',
-  line    => 'PasswordAuthentication no',
-  match   => '^PasswordAuthentication',
-  replace => true,
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
 }
