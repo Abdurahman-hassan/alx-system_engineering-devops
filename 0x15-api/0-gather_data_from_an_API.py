f302c23bdf213dcd6377fbeaa17f79f04a0467c9
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-03-26 02:51:11 +0200

    a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.
    Requirements:
    
    You must use urllib or requests module
    The script must accept an integer as a parameter, which is the employee ID
    The script must display on the standard output the employee TODO list progress in this exact format:
    First line: Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
    EMPLOYEE_NAME: name of the employee
    NUMBER_OF_DONE_TASKS: number of completed tasks
    TOTAL_NUMBER_OF_TASKS: total number of tasks, which is the sum of completed and non-completed tasks
    Second and N next lines display the title of completed tasks: TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
+    response = requests.get(url)
+    user = response.json()
+    name = user.get('name')
+    url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1])
+    response = requests.get(url)
+    todos = response.json()
+    done = [todo for todo in todos if todo.get('completed') is True]
+    print('Employee {} is done with tasks({}/{}):'.format(name, len(done), len(todos)))
+    for todo in done:
+        print('\t {}'.format(todo.get('title')))
---
+++ 0x15-api/README.md
@@ -0,0 +1,2 @@
+# This is an 0x15-api project
+```
