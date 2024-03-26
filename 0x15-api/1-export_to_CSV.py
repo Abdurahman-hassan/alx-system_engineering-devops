7c94ff0adab0e45202e2b0f3607d97f6b0e2bc17
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-03-26 03:01:23 +0200

    change the script

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
 """Gather data from an API"""
 import csv
-import requests
 from sys import argv
 
+import requests
+
 if __name__ == "__main__":
-    url = f'https://jsonplaceholder.typicode.com/users/{argv[1]}'
+    user_id = argv[1]
+    url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
     response = requests.get(url)
     user = response.json()
-    name = user.get('name')
-    url = f'https://jsonplaceholder.typicode.com/todos?userId={argv[1]}'
+    username = user.get('username')  # Ensure you are getting 'username', not 'name'
+
+    url = f'https://jsonplaceholder.typicode.com/todos?userId={user_id}'
     response = requests.get(url)
     todos = response.json()
-    with open(f'{argv[1]}.csv', 'w', newline='') as csvfile:
+
+    with open(f'{user_id}.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for todo in todos:
             writer.writerow([
-                argv[1],
-                name,
+                user_id,
+                username,
                 todo.get('completed'),
                 todo.get('title')
             ])
